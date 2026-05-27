e7f11ca706eb44eb2b3428813f2c212a9fb604c2
JamonSmith <127971432+JamonSmith@users.noreply.github.com>
2026-05-26 20:40:32 -0500

    Add files via upload

---
+++ generate_rgb.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+numPts = 5000
+
+rgbVals = np.random.randint(0, 256, size = (numPts, 3))
+
+df = pd.DataFrame(rgbVals, columns = ["R", "G", "B"])
+
+df.to_csv("rgb_data.csv", index = False)
